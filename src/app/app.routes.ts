5be567550e6c05ea128f270c0f4b2f0419bdfcbe
Pallavi Lawate <pallavi_lawate@magicsoftware.com>
2025-11-28 14:44:51 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home';
+import { Service } from './components/service';
+import { Contact } from './components/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', component: Home },
+	{ path: 'service', component: Service },
+	{ path: 'contact', component: Contact },
+	{ path: '**', redirectTo: '' }
+];
---
+++ src/app/components/service.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service',
+  standalone: true,
+  templateUrl: './service.html',
+  styleUrl: './service.scss'
+})
+export class Service {}
